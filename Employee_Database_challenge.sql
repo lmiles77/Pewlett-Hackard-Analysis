82b441a3e8f95dde9591e205cfca941d55e978a9
lmiles77 <80069183+lmiles77@users.noreply.github.com>
2021-05-02 20:02:44 -0500

    Update Employee_Database_challenge.sql

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+# Create columns with the folowing data
 SELECT
 	e.emp_no,
 	e.first_name,
@@ -5,6 +6,7 @@ SELECT
 	ti.title,
 	ti.from_date,
 	ti.to_date
+# create retirement titles table based on birthday year
 INTO retirement_titles
 	FROM employees as e
 	JOIN titles AS ti
@@ -15,6 +17,7 @@ INTO retirement_titles
 
 SELECT * FROM retirement_titles
 
+# Get list of unique titles of employees
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 	rt.first_name,
  	rt.last_name,
@@ -25,6 +28,7 @@ INTO unique_titles
 
 SELECT * FROM unique_titles
 
+# Get count of retiring titles
 SELECT COUNT (title), 
 	title
 	INTO retiring_titles
@@ -32,3 +36,26 @@ SELECT COUNT (title),
 	GROUP BY title
 	ORDER BY count desc;
 SELECT * FROM retiring_titles
+
+# Employees Eligible to participate in Mentorship Program
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+	INTO mentor_eligibility
+	FROM employees AS e
+	INNER JOIN dept_emp AS de
+	ON (e.emp_no = de.emp_no)
+	INNER JOIN titles AS ti
+	ON (e.emp_no = ti.emp_no)
+	WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+	ORDER BY emp_no;
+
+SELECT * FROM mentor_eligibility
+
+
+
